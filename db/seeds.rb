77b7011292e75e65fc16e2689cce4be80b2afe54
vagrant <vagrant@kellogg-dev-box>
2014-04-19 21:48:43 +0000

    Revised Book/list ver2.1 updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+Diceroll.create(:face => 1, :url => "http://kiei925.com/images/dice/1.png,")
+Diceroll.create(:face => 2, :url => "http://kiei925.com/images/dice/2.png,")
+Diceroll.create(:face => 3, :url => "http://kiei925.com/images/dice/3.png,")
+Diceroll.create(:face => 4, :url => "http://kiei925.com/images/dice/4.png,")
+Diceroll.create(:face => 5, :url => "http://kiei925.com/images/dice/5.png,")
+Diceroll.create(:face => 6, :url => "http://kiei925.com/images/dice/6.png,")
+
+
+
 Book.create(:title => "A Brief History of Time", :author => "Stephen Hawking")
 Book.create(:title => "Alice in Wonderland", :author => "Lewis Carroll")
 Book.create(:title => "The Lord Of The Rings", :author => "J.R.R. Tolkien")
