ca32225f3e8cbab58c1bc832b5d5654e9cbc5384
vagrant <vagrant@kellogg-dev-box>
2014-05-13 04:59:57 +0000

    Homework#6 ver2

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513023051) do
+ActiveRecord::Schema.define(version: 20140513044215) do
 
   create_table "books", force: true do |t|
     t.string  "title"
     t.string  "author"
     t.integer "price"
+    t.integer "avg_rating"
   end
 
   create_table "dicerolls", force: true do |t|
